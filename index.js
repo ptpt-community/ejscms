f2337fd733b716b99bdaece856ebd708a866bc3f
Jisan <jishananam@iut-dhaka.edu>
2023-04-29 21:17:59 +0600

    First

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 let ejs = require('ejs');
 const fs = require('fs').promises;
+const simpleGit = require('simple-git');
+
 
 let configuration;
 /*
@@ -7,29 +9,62 @@ let configuration;
  * configuratioon.staticHome
  */
 
-function configure (config){
-   configuration = config;
+
+
+function configure(config) {
+    configuration = config;
 }
+function nodeLibraryGit() {
+    const path = '/media/jisan/C48458B38458A9A6/8th semester/Thesis/cms-dbms-showcase/static-home/home/index.html';
+    const message='okay'
+    const folderPath='/media/jisan/C48458B38458A9A6/8th semester/Thesis/cms-dbms-showcase/static-home/home'
 
-async function commit(templatePath, data, routePath){
+    simpleGit(folderPath).init((err, response) => {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log('Repository initialized at ' + JSON.stringify(response));
+            // Create a file to commit
+            // Stage the file for commit
+            simpleGit().add(path, (err) => {
+                if (err) {
+                    console.error(err);
+                } else {
+                    console.log('File staged for commit');
+                    // Commit the changes
+                    simpleGit().commit('Initial commit', (err) => {
+                        if (err) {
+                            console.error(err);
+                        } else {
+                            console.log('Changes committed');
+                        }
+                    });
+                }
+            });
+        }
+    });
+
+
+}
+async function commit(templatePath, data, routePath) {
     try {
-    let outputPath = configuration.staticHome+'/'+routePath+'/';
-    let template = await fs.readFile(templatePath, 'utf8');
-    let html = ejs.render(template, data);
-    
-    await fs.mkdir(outputPath, {recursive:true});
-    await fs.writeFile(outputPath+"/index.html", html)
+        let outputPath = configuration.staticHome + '/' + routePath + '/';
+        let template = await fs.readFile(templatePath, 'utf8');
+        let html = ejs.render(template, data);
 
+        await fs.mkdir(outputPath, {recursive: true});
+        await fs.writeFile(outputPath + "/index.html", html)
 
-    }
-    catch(e){
+        nodeLibraryGit()
+
+    } catch (e) {
         console.log(e);
     }
 
 }
 
 
-module.exports={
+module.exports = {
     configure, commit
 
 }
--- test.js
+++ test.js
@@ -1,8 +1,11 @@
 cms = require("./index.js")
 
 conf = {
-    staticHome: "/home/labnan/start/more-projects/thesis/ejscms/temps/static"
+    staticHome: "/media/jisan/C48458B38458A9A6/8th semester/Thesis/Static"
 }
 let data = [1,2,3];
 cms.configure(conf);
-cms.commit("/home/labnan/start/more-projects/thesis/ejscms/temps/templates/hello.ejs", {data}, "home");
+cms.commit("/media/jisan/C48458B38458A9A6/8th semester/Thesis/Templates/page.ejs", {data}, "home");
+
+// Open the Git repository
+
